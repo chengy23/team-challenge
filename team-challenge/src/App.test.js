9fe349eb1e619a797f904af519f598b3e20c6af2
Yi Cheng <chengy23@uw.edu>
2016-11-28 15:17:47 -0800

    modified name field test

--- team-challenge/src/App.test.js
+++ team-challenge/src/App.test.js
@@ -14,7 +14,7 @@ describe('<RequiredInput /> name component', () => {
   it("should throw error message when name field is empty", () => {
     const wrapper = shallow(<RequiredInput errorMessage={"we need to know your name"} value={''}/>);
     //expect to throw error message when user didn't type name
-    expect(wrapper.find('.error-missing').text()).toEqual("we need to know your name");
+    expect(wrapper.find('.error-missing').text()).toEqual('we need to know your name');
   });
 
   it("should do nothing when the name field is not empty", () => {
@@ -24,34 +24,34 @@ describe('<RequiredInput /> name component', () => {
   });
 });
 
-//test if the password field is empty
-describe('<RequiredInput /> password component', () => {
-   it("should throw error message when password field is empty", () => {
-    const wrapper = shallow(<RequiredInput errorMessage={"your password can't be blank"} value={''}/>);
-    //expect to throw error message when user didn't type their password
-    expect(wrapper.find('.error-missing').text()).toEqual("your password can't be blank");
-
-  });
-  
-  it("should do nothing when the password field is not empty", () => {
-    const wrapper = shallow(<RequiredInput value={'123'} />);
-    //expect that there is no error message
-    expect(wrapper.contains('.error-missing')).toEqual(false);
-  });
-});
-
-//test if the password confirmation input match the password
-// describe('<PasswordConfirmationInput /> component', () => {
-//    it("should throw error message when the password confirmation does not match the password", () => {
-//     const wrapper = shallow(<PasswordConfirmationInput errorMessage={"password don't match"} value={'password'}/>);
+// //test if the password field is empty
+// describe('<RequiredInput /> password component', () => {
+//    it("should throw error message when password field is empty", () => {
+//     const wrapper = shallow(<RequiredInput errorMessage={"your password can't be blank"} value={''}/>);
 //     //expect to throw error message when user didn't type their password
-//     expect(wrapper.find('.error-mismatched').text()).toEqual("passwords don't match");
-
+//     expect(wrapper.find('.error-missing').text()).toEqual("your password can't be blank");
 //   });
   
-  // it("should do nothing when the password field is not empty", () => {
-  //   const wrapper = shallow(<PasswordConfirmationInput value={'123'} />);
-  //   //expect that there is no error message
-  //   expect(wrapper.contains('.error-missing')).toEqual(false);
-  // });
-});
+//   it("should do nothing when the password field is not empty", () => {
+//     const wrapper = shallow(<RequiredInput value={'123'} />);
+//     //expect that there is no error message
+//     expect(wrapper.contains('.error-missing')).toEqual(false);
+//   });
+
+//   //test if the password confirmation input match the password input
+//   describe('<PasswordConfirmationInput /> component', () => {
+//     it("should throw error message when the password confirmation does not match the password", () => {
+//       const passwordConf = shallow(<PasswordConfirmationInput password={'123'} errorMessage={"password don't match"} value={'12'}/>);
+//       //expect to throw error message when the password confirmation does not match the password
+//       expect(passwordConf.find('.error-mismatched').text()).toEqual("passwords don't match");
+//     });
+    
+//     it("should do nothing when the password confirmation field match the password field", () => {
+//       const passwordConf = shallow(<PasswordConfirmationInput password={'12'} errorMessage={"password don't match"} value={'12'}/>);
+//       //expect to throw error message when the password confirmation does not match the password
+//       expect(passwordConf.find('.error-mismatched').text()).toEqual("passwords don't match");
+//       //expect that there is no error message
+//       expect(passwordConf.contains('.error-mismatched')).toEqual(false);
+//     });
+//   });
+// });

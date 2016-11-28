a5b2831a88a7fa1e12b630c2e25a06516cd79c87
leontaolong <longt8@uw.edu>
2016-11-28 15:20:42 -0800

    Close #2-test email address field

--- team-challenge/src/App.test.js
+++ team-challenge/src/App.test.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {shallow} from 'enzyme';;
+import {EmailInput} from './TeamSignUp';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+// this test checks the <EmailInput /> component:
+// if it pops up error message properly if it's either empty or invalid
+describe('<EmailInput /> component', () => {
+
+  // expect that it shows a "require email" message and no "invalid email" message
+  it('should warn "need email address" if empty', () => {
+    const wrapper = shallow(<EmailInput value={''}/>);
+    expect(wrapper.find('.error-missing').text()).toEqual('we need to know your email address')
+    || expect(wrapper.find('.error-invalid').length).toEqual(0); 
+  });
+
+  // expect that it shows an "invalid email" message and no "require email" message
+  it('should warn "email address invalid" if invalid', () => {
+    const wrapper = shallow(<EmailInput value={'uw-ischool.com'}/>);
+    expect(wrapper.find('.error-invalid').text()).toEqual('this is not a valid email address')
+    || expect(wrapper.find('.error-missing').length).toEqual(0);
+  });
+
+  // expect that it doesn't show any error message
+  it('should not warn anything if neither empty or invalid', () => {
+    const wrapper = shallow(<EmailInput value={'ischool@uw.edu'}/>);
+    expect(wrapper.find('.help-block').length).toEqual(0);
+  });
 });
+
+
